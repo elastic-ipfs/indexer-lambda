c5bac9855e809a1017eb022f1a01fb065853ab35
Shogun <paolo@cowtech.it>
2022-02-24 11:37:21 +0100

    feat: Vastly improve telemetry.

--- src/telemetry.js
+++ src/telemetry.js
@@ -1,69 +1,126 @@
 'use strict'
 
-const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus')
-const { MeterProvider } = require('@opentelemetry/sdk-metrics-base')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const { load } = require('js-yaml')
+const { MeterProvider, UngroupedProcessor } = require('@opentelemetry/sdk-metrics-base')
+const { hrTime } = require('@opentelemetry/core')
+const { logger } = require('./logging')
 
-const exporter = new PrometheusExporter({ preventServerStart: true })
-const meters = {}
-const metrics = {}
+class Aggregator {
+  constructor(sum = true) {
+    this.sum = sum
 
-// Create all the metrics
-meters.s3Meter = new MeterProvider({ exporter, interval: 100 }).getMeter('s3')
-metrics.s3Fetchs = meters.s3Meter.createCounter('s3-fetchs', { description: 'Fetchs on S3' })
-metrics.s3FetchsDurations = meters.s3Meter.createCounter('s3-fetchs-durations', {
-  description: 'Fetchs durations on S3'
-})
+    this.reset([0, 0])
+  }
 
-meters.dynamoMeter = new MeterProvider({ exporter, interval: 100 }).getMeter('dynamo')
+  update(value) {
+    this.lastUpdate = hrTime()
 
-metrics.dynamoReads = meters.dynamoMeter.createCounter('dynamo-reads', { description: 'Reads on DynamoDB' })
-metrics.dynamoReadsDurations = meters.dynamoMeter.createCounter('dynamo-reads-durations', {
-  description: 'Reads durations on DynamoDB'
-})
+    if (this.sum) {
+      this.value += value
+    } else {
+      this.value.push(value)
+    }
+  }
 
-metrics.dynamoCreates = meters.dynamoMeter.createCounter('dynamo-creates', { description: 'Creates on DynamoDB' })
-metrics.dynamoCreatesDurations = meters.dynamoMeter.createCounter('dynamo-creates-durations', {
-  description: 'Creates durations on DynamoDB'
-})
+  reset(time) {
+    this.value = this.sum ? 0 : []
+    this.lastUpdate = time || hrTime()
+  }
 
-metrics.dynamoUpdates = meters.dynamoMeter.createCounter('dynamo-updates', { description: 'Updates on DynamoDB' })
-metrics.dynamoUpdatesDurations = meters.dynamoMeter.createCounter('dynamo-updates-durations', {
-  description: 'Updates durations on DynamoDB'
-})
+  toPoint() {
+    const point = {
+      value: this.value,
+      timestamp: this.lastUpdate
+    }
 
-metrics.dynamoDeletes = meters.dynamoMeter.createCounter('dynamo-deletes', { description: 'Deletes on DynamoDB' })
-metrics.dynamoDeletesDurations = meters.dynamoMeter.createCounter('dynamo-deletes-durations', {
-  description: 'Deletes durations on DynamoDB'
-})
+    this.reset()
 
-meters.sqsMeter = new MeterProvider({ exporter, interval: 100 }).getMeter('sqs')
-metrics.sqsPublishes = meters.sqsMeter.createCounter('sqs-publishes', { description: 'Publishes on SQS' })
-metrics.sqsPublishesDurations = meters.sqsMeter.createCounter('sqs-publishes-durations', {
-  description: 'Publishes durations on SQS'
-})
-
-async function trackDuration(metric, promise) {
-  const startTime = process.hrtime.bigint()
-
-  try {
-    return await promise
-  } finally {
-    metric.add(Number(process.hrtime.bigint() - startTime) / 1e6)
+    return point
   }
 }
 
-function storeMetrics() {
-  /* c8 ignore next 3 */
-  if (!exporter._batcher.hasMetric) {
-    return
+class Processor extends UngroupedProcessor {
+  aggregatorFor(metricDescriptor) {
+    return new Aggregator(!metricDescriptor.name.endsWith('-durations'))
+  }
+}
+
+class Telemetry {
+  constructor() {
+    const { component, interval, metrics } = load(readFileSync(join(process.cwd(), 'metrics.yml'), 'utf-8'))
+
+    this.component = component
+    this.logger = logger
+    this.meter = new MeterProvider({ exporter: this, interval, processor: new Processor() }).getMeter(component)
+    this.metrics = {}
+    for (const [category, description] of Object.entries(metrics)) {
+      this.createMetric(category, description, 'count')
+      this.createMetric(category, description, 'durations')
+    }
   }
 
-  return exporter._serializer.serialize(exporter._batcher.checkPointSet())
+  increaseCount(category, amount = 1) {
+    const metric = this.ensureMetric(category, 'count')
+    metric.add(amount)
+  }
+
+  decreaseCount(category, amount = 1) {
+    const metric = this.ensureMetric(category, 'count')
+    metric.add(-1 * amount)
+  }
+
+  async trackDuration(category, promise) {
+    const metric = this.ensureMetric(category, 'durations')
+    const startTime = process.hrtime.bigint()
+
+    try {
+      return await promise
+    } finally {
+      metric.add(Number(process.hrtime.bigint() - startTime) / 1e6)
+    }
+  }
+
+  async flush() {
+    await this.meter.collect()
+    this.export(this.meter.getProcessor().checkPointSet())
+  }
+
+  createMetric(category, description, metric, creator = 'createCounter') {
+    const tag = `${category}-${metric}`
+    this.metrics[tag] = this.meter[creator](tag, { description: `${description} (${metric})` })
+  }
+
+  ensureMetric(category, metric) {
+    const metricObject = this.metrics[`${category}-${metric}`]
+
+    if (!metricObject) {
+      throw new Error(`Metric ${category} not found`)
+    }
+
+    return metricObject
+  }
+
+  // Implements https://github.com/open-telemetry/opentelemetry-js/blob/v0.26.0/experimental/packages/opentelemetry-sdk-metrics-base/src/export/types.ts#L100
+  export(records, done) {
+    this.logger.info(
+      {
+        ipfs_provider_component: this.component,
+        metrics: Object.fromEntries(records.map(r => [r.descriptor.name, r.aggregator.toPoint().value]))
+      },
+      'Dumping metrics ...'
+    )
+
+    if (done) {
+      done('SUCCESS')
+    }
+  }
+
+  // Implements https://github.com/open-telemetry/opentelemetry-js/blob/v0.26.0/experimental/packages/opentelemetry-sdk-metrics-base/src/export/types.ts#L100
+  async shutdown() {
+    // No-op
+  }
 }
 
-module.exports = {
-  meters,
-  metrics,
-  storeMetrics,
-  trackDuration
-}
+module.exports = new Telemetry()
--- test/miscellaneous.test.js
+++ test/miscellaneous.test.js
@@ -5,7 +5,8 @@ process.env.LOG_LEVEL = ''
 process.env.NODE_DEBUG = 'aws-ipfs-indexing-lambda'
 
 const t = require('tap')
-const { elapsed } = require('../src/logging')
+const { logger, elapsed } = require('../src/logging')
+const telemetry = require('../src/telemetry')
 
 t.test('logging - elapsed times are correctly evaluated', t => {
   t.plan(3)
@@ -14,4 +15,46 @@ t.test('logging - elapsed times are correctly evaluated', t => {
   t.not(elapsed(start), '0')
   t.not(elapsed(start, 2, 'nanoseconds'), '0')
   t.equal(elapsed(start, 0, 'seconds'), '0')
+})
+
+t.test('telemetry - correctly implements interfaces', async t => {
+  t.plan(5)
+
+  telemetry.createMetric('custom', 'Custom', 'count', 'createUpDownCounter')
+
+  // Set the logger to check the tracking
+  telemetry.logger = {
+    info(arg) {
+      t.strictSame(arg, { ipfs_provider_component: 'indexer-lambda', metrics: { 'custom-count': 1 } })
+    }
+  }
+
+  telemetry.increaseCount('custom')
+  telemetry.increaseCount('custom')
+  telemetry.decreaseCount('custom')
+  await telemetry.flush()
+
+  // Set the logger to check the refresh
+  telemetry.logger = {
+    info(arg) {
+      t.strictSame(arg, { ipfs_provider_component: 'indexer-lambda', metrics: { 'custom-count': 0 } })
+    }
+  }
+
+  await telemetry.flush()
+
+  // Now check other methods
+  telemetry.logger = {
+    info(arg) {}
+  }
+
+  t.throws(() => telemetry.decreaseCount('unknown'), 'Metrics unknown not found.')
+  await t.resolves(() => telemetry.shutdown())
+
+  telemetry.export([], argument => {
+    t.equal(argument, 'SUCCESS')
+  })
+
+  // Reset the logger
+  telemetry.logger = logger
 })
