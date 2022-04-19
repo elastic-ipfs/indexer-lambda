adde8e66b653a82d23fc8cb106ae15e3226b04c5
Francisco Cardoso <francardoso@outlook.com>
2022-04-19 16:02:27 -0300

    Potencial fix for indexer update errors

--- src/index.js
+++ src/index.js
@@ -181,8 +181,14 @@ async function main(event) {
             if (allCars.size === 1 && allCars.has(carId)) {
               await deleteDynamoItem(blocksTable, primaryKeys.blocks, cidToKey(block.cid))
             } else {
-              await appendCarToBlock(block, existingBlock.cars, carId)
-              await updateCarStatus(carId, block)
+              /* This is required for avoiding DynamoDB error:
+              "Item size to update has exceeded the maximum allowed size" */
+              if (allCars.size < 300) {
+                await appendCarToBlock(block, existingBlock.cars, carId)
+                await updateCarStatus(carId, block)
+              } else {
+                logger.warn(`Existing block ${cidToKey(block.cid)} already has more then 300 items at CARs attribute list. Current CAR ${carId} won't be appended`)
+              }
               return
             }
           }
