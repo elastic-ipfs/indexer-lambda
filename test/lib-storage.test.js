03548c74a5b9b9f97bd84f50d5670568e15e01c9
Simone Sanfratello <simone.sanfratello@nearform.com>
2022-10-18 16:12:33 +0200

    fix: dynamo batch write unprocessed (#70)
    * chore: update deps - strict version
    
    * fix: handle unprocessed items in dynamo write batch
    
    * fix: lint

--- test/lib-storage.test.js
+++ test/lib-storage.test.js
@@ -48,6 +48,90 @@ t.test('batchWriteDynamoItems', async t => {
     await batchWriteDynamoItems({ batch })
   })
 
+  t.test('should handle unprocessed items from batch write response', async t => {
+    const unprocessedItems =
+      { 'prod-ep-v1-blocks': [{ PutRequest: { Item: { type: { S: 'raw' }, createdAt: { S: '2022-10-16T20:34:14.465Z' }, multihash: { S: 'z2DrjgbByyopTuqrKjRhhSJWksbK3FcfxicDvBav7gPzoXDfWdf' } } } }] }
+
+    const firstRequest = {
+      table1: [{ PutRequest: { Item: { block: { S: 'abc' }, car: { S: '123' }, offset: { N: '1' }, length: { N: '2' } } } }],
+      table2: [{ PutRequest: { Item: { block: { S: 'abc' }, type: { S: 'raw' } } } }]
+    }
+
+    dynamoMock.on(BatchWriteItemCommand, { RequestItems: firstRequest })
+      .resolves({ UnprocessedItems: unprocessedItems })
+
+    dynamoMock.on(BatchWriteItemCommand, { RequestItems: unprocessedItems })
+      .resolves({})
+
+    const messages = { debug: [], error: [], warn: [] }
+    const loggerSpy = {
+      debug: (_, message) => messages.debug.push(message),
+      warn: (_, message) => messages.warn.push(message),
+      error: (_, message) => messages.error.push(message)
+    }
+    const batch = [
+      {
+        table: 'table1',
+        items: [{ block: 'abc', car: '123', offset: 1, length: 2 }]
+      },
+      {
+        table: 'table2',
+        items: [{ block: 'abc', type: 'raw' }]
+      }]
+
+    await batchWriteDynamoItems({ batch, logger: loggerSpy })
+
+    t.equal(messages.debug.length, 0)
+    t.equal(messages.error.length, 0)
+    t.equal(messages.warn.length, 1)
+    t.equal(messages.warn[0], 'UnprocessedItems in BatchWriteItemCommand')
+  })
+
+  t.test('should handle more unprocessed items from batch write response', async t => {
+    const unprocessedItems1 =
+      { 'prod-ep-v1-blocks': [{ PutRequest: { Item: { type: { S: 'raw' }, createdAt: { S: '2022-10-16T20:34:14.465Z' }, multihash: { S: 'hash1' } } } }] }
+    const unprocessedItems2 =
+      { 'prod-ep-v1-blocks': [{ PutRequest: { Item: { type: { S: 'raw' }, createdAt: { S: '2022-10-16T20:34:14.465Z' }, multihash: { S: 'hash2' } } } }] }
+
+    const firstRequest = {
+      table1: [{ PutRequest: { Item: { block: { S: 'abc' }, car: { S: '123' }, offset: { N: '1' }, length: { N: '2' } } } }],
+      table2: [{ PutRequest: { Item: { block: { S: 'abc' }, type: { S: 'raw' } } } }]
+    }
+
+    dynamoMock.on(BatchWriteItemCommand, { RequestItems: firstRequest })
+      .resolves({ UnprocessedItems: unprocessedItems1 })
+
+    dynamoMock.on(BatchWriteItemCommand, { RequestItems: unprocessedItems1 })
+      .resolves({ UnprocessedItems: unprocessedItems2 })
+
+    dynamoMock.on(BatchWriteItemCommand, { RequestItems: unprocessedItems2 })
+      .resolves({})
+
+    const messages = { debug: [], error: [], warn: [] }
+    const loggerSpy = {
+      debug: (_, message) => messages.debug.push(message),
+      warn: (_, message) => messages.warn.push(message),
+      error: (_, message) => messages.error.push(message)
+    }
+    const batch = [
+      {
+        table: 'table1',
+        items: [{ block: 'abc', car: '123', offset: 1, length: 2 }]
+      },
+      {
+        table: 'table2',
+        items: [{ block: 'abc', type: 'raw' }]
+      }]
+
+    await batchWriteDynamoItems({ batch, logger: loggerSpy })
+
+    t.equal(messages.debug.length, 0)
+    t.equal(messages.error.length, 0)
+    t.equal(messages.warn.length, 2)
+    t.equal(messages.warn[0], 'UnprocessedItems in BatchWriteItemCommand')
+    t.equal(messages.warn[1], 'UnprocessedItems in BatchWriteItemCommand')
+  })
+
   t.test('writeDynamoItem - reports DynamoDB errors', async t => {
     dynamoMock.on(BatchWriteItemCommand).rejects(new Error('FAILED'))
     const messages = { debug: [], error: [] }
